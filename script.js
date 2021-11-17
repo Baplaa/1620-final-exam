71820492729d158b388b0d7e428dcc38ae6ab253
satonohime <nananariru@gmail.com>
2021-11-17 13:00:20 -0800

    finished renderIndex, added temporary test data for functions

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+const contactList = [  
+	{ 
+		name: "Oliver Queen", 
+		phone: "778-555-1234", 
+		address: "101 Main St, Star City, USA",    
+		email: "greenarrow@watchtower.com",  
+	},   
+	{    
+		name: "Jessica Cruz",    
+		phone: "123-555-5555",    
+		address: "Portland Oregon",    
+		email: "greenlantern@watchtower.com",  
+	}
+]
+
+
 function cleanUpIndex() {
-    let eleList = document.querySelectorAll('.contact')
+    let eleList = document.querySelector('.main')
     
-    for (let i = 0; i < eleList.length; i++) {
-        eleList[i].remove()
-    }
+    eleList.innerHTML = ""
 }
 
-function renderIndex(contactArray) {
 
+function renderIndex(contactArray) {
+    let body = document.querySelector('.main')
+    for (let i = 0; i < contactArray.length; i++) {
+        body.innerHTML += '<a href="page3.html"><div class="contact">' + contactArray[i].name + '</div></a>'
+    }
 }
