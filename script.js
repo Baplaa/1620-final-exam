bdd8b68051d264530898bf6a87f26f4842ee04ac
satonohime <nananariru@gmail.com>
2021-11-17 14:29:56 -0800

    finished: renderView, createSingleIndex

--- script.js
+++ script.js
@@ -20,11 +20,14 @@ function cleanUpIndex() {
     mainContent.innerHTML = ""
 }
 
+function createSingleIndex(contact) {
+    return `<a href="page3.html"><div class="contact">${contact.name}</div></a>`
+}
 
 function renderIndex(contactArray) {
-    let body = document.querySelector('.main')
+    let mainContent = document.querySelector('.main')
     for (let i = 0; i < contactArray.length; i++) {
-        body.innerHTML += '<a href="page3.html"><div class="contact">' + contactArray[i].name + '</div></a>'
+        mainContent.innerHTML += createSingleIndex(contactArray[i])
     }
 }
 
@@ -35,5 +38,50 @@ function cleanUpView() {
 }
 
 function renderView() {
+    let mainContent = document.querySelector('.main')
+    
+    let inputTypeList = ['text', 'tel', 'text', 'email']
+    let placeholderList = ['Name', 'Phone', 'Address', 'Email']
+	let fieldTypeList = placeholderList.slice(0)
+
+	for (let i = 0; i < fieldTypeList.length; i++) (
+		fieldTypeList[i] = fieldTypeList[i].toLowerCase()
+	)
+
+    let inputContainerList = []
+
+	for (let i = 0; i < 4; i++) {
+		inputContainerList[i] = 
+		`<div class="inputcontainer">
+			<input type="${inputTypeList[i]}" id="contact${fieldTypeList[i]}" name="contact${fieldTypeList[i]}" placeholder="Contact ${placeholderList[i]}">
+			<button class="extrafield" id="extra${fieldTypeList[i]}field" name="extra${fieldTypeList[i]}field">+</button>
+		</div>`
+	}
+
+	mainContent.innerHTML += 
+	`<div class="contactedit">
+		<div class="contactimg">
+			<img src="./img/profile.jpg" class ="profilepic" alt="Profile picture">
+		</div>
+		<div class="form">
+			<form>
+			</form>
+		</div>	
+	</div>`
+
+	let formContent = document.querySelector('form')
+
+	for (let i = 0; i < inputContainerList.length; i++) {
+		formContent.innerHTML += inputContainerList[i]
+	}
+
+	let page2ButtonHTML = 
+	`<div class="buttons">
+		<button type="submit" class="button save" id="savecontact" name="savecontact">Save Contact</button>
+		<button type="reset" class="button cancel" id="cancel" name="cancel">Cancel</button>
+	</div>`
+
+	formContent.insertAdjacentHTML('beforeend', page2ButtonHTML)
+
 
 }
